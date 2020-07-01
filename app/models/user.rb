ed41967093ce671b73ed87a1cf95d6a9b6dfab24
themaleem <zizo9585@gmail.com>
2020-07-01 23:18:47 +0100

    initial commit with devise and milia added

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :registerable,
+         :confirmable,
+         :recoverable,
+         :rememberable,
+         :validatable
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # devise :database_authenticatable, :registerable, :confirmable, :confirmable,
+  #        :recoverable, :rememberable, :validatable
+end
